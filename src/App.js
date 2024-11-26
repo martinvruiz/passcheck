c2a687a61b4968e2cbfa808048946b31f22d9ae3
¨martinvruiz¨ <martinvruiz10@gmail.com>
2024-11-26 10:30:52 -0300

    est`

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         title="Como probar tus contraseñas?" 
         text1="¿Sabías que una buena contraseña puede hacer toda la diferencia para proteger tus cuentas? En este lugar podes probar para que sean únicas, súper seguras y fáciles de recordar." 
         text2="En el rectangulo principal tenes un recuadro para probar las contraseñas que uses diariamente. Abajo de ese recuadro, tenes las principales combinaciones para hacer tu/s contraseña/s mas seguras. Cuando cumplas alguna de estas, se podra de color verde, mientras mas cumplas mas segura tu contraseña!" 
-        text3="No guardamos ningun tipo de dato, la app solo chequea que tu contraseña cumpla con estos requisitos. No hay un limite para probar estas, podes dejar volar tu imaginacion para que sean seguras pero no olvides que tienen que ser faciles de recordar!"
+        text3="No guardamos ningun tipo de dato, la app solo chequea que tu contraseña cumpla con estos requisitos. No hay un limite para probar estas, podes dejar volar tu imaginacion para que sean seguras!"
         />
       </section>
     </div>
--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -5,8 +5,8 @@ export const Info = ({text1,text2,text3, title})=>{
     return <div className="bg-white rounded-xl text-center w-2/5 m-2 p-2">
 
         <h2 className="text-2xl font-bold my-1">{title}</h2>
-        <p className="p-2">{text1}</p>
-        <p className="p-2">{text2}</p>
-        <p className="p-2">{text3}</p>
+        <p className="p-4">{text1}</p>
+        <p className="p-4">{text2}</p>
+        <p className="p-4">{text3}</p>
     </div>
 }
